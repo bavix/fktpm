1186d21898ed73384e0a9ed126ad29f2f58d75fa
Babichev Maxim <info@babichev.net>
2017-10-11 16:56:55 +0300

    patch revs

--- app/Models/Rev.php
+++ app/Models/Rev.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Auth;
 
 class Rev extends Model
 {
-    protected $table = 'revs';
-    public $timestamps = false;
+    protected $table      = 'revs';
+    public    $timestamps = false;
 
     public static function makeRev(Model $model, $newData, $column = 'content')
     {
@@ -18,21 +18,23 @@ class Rev extends Model
         if ($model->id && $old && \function_exists('xdiff_string_rabdiff'))
         {
             // revs
-            $rev          = new static();
-            $rev->patch   = Diff::diff($newData, $old);
-            $rev->name    = $model->getTable();
-            $rev->item_id = $model->id;
+            $rev           = new static();
+            $rev->patch    = Diff::diff($newData, $old);
+            $rev->name     = $model->getTable();
+            $rev->column   = $column;
+            $rev->item_id  = $model->id;
             $rev->admin_id = Auth::guard('admin')->user()->id;
             $rev->save();
             // /revs
         }
     }
 
-    public static function fromModel(Model $model)
+    public static function fromModel(Model $model, $column = 'content')
     {
         return static::query()
             ->where('name', $model->getTable())
             ->where('item_id', $model->id)
+            ->where('content', $column)
             ->orderBy('created_at', 'DESC')
             ->get();
     }
--- database/migrations/2017_10_11_154514_create_revs_table.php
+++ database/migrations/2017_10_11_154514_create_revs_table.php
@@ -18,11 +18,14 @@ class CreateRevsTable extends Migration
 
             $table->binary('patch'); // diff results
             $table->string('name'); // table name ($model->getTable())
+            $table->string('column'); // table name ($model->getTable())
             $table->integer('item_id'); // id content message
             $table->integer('admin_id'); // id content message
 
             $table->timestamp('created_at')
                 ->default(DB::raw('CURRENT_TIMESTAMP'));
+
+            $table->index(['name', 'column', 'item_id']);
         });
     }
 
