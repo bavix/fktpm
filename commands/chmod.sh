d441107ae776ae2b4ef06438da98eaf7ca45e33c
Babichev Maxim <info@babichev.net>
2017-10-15 20:28:21 +0300

    + chmod sh

---
+++ commands/chmod.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cd ..
+chmod 777 bootstrap
+chmod 777 bootstrap/cache
+chmod 777 storage/logs
+chmod a+x storage/logs/laravel.log
+chmod 777 storage/purifier
+chmod 777 storage/framework/cache
+chmod 777 storage/framework/sessions
+chmod 777 storage/framework/views
+chmod 777 public/upload -R
