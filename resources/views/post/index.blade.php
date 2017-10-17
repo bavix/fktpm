73c602e940c2405547732e5c105f9e53a6efb34a
Babichev Maxim <info@babichev.net>
2017-10-17 17:48:07 +0300

    patch template

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -10,20 +10,23 @@
 
                 <article class="card">
 
-                    @if ($item->image_id)
-                        <div class="card-img-top">
-                            <a href="{{ $item->url() }}" title="{{ $item->title }}">
-                                <img class="img-fluid" src="/upload/{{ $item->image->preview() }}" title="{{ $item->title }}">
-                            </a>
-                        </div>
+                    @if (method_exists($item, 'image') && $item->image_id)
+                        <a class="card-img-top text-center" href="{{ $item->url() }}" title="{{ $item->title }}">
+                            <img class="img-fluid" src="/upload/{{ $item->image->preview() }}" title="{{ $item->title }}">
+                        </a>
                     @endif
 
                     <div class="card-body">
 
-                        <small class="text-muted">
-                            Добавлено
-                            <time datetime="{{ $item->created_at }}">{{ \diffForHumans($item->created_at) }}</time>
-                        </small>
+                        @if(method_exists($item, 'category'))
+                            <div class="card-text">
+                                <small class="text-muted">
+                                    Категория:
+                                    <a href="{{ $item->category->url() }}"
+                                       title="{{ $item->category->title }}" >{{ $item->category->title }}</a>
+                                </small>
+                            </div>
+                        @endif
 
                         <h3 class="card-title">
                             <a href="{{ $item->url() }}" title="{{ $item->title }}">
@@ -35,15 +38,12 @@
                             <p class="card-text">{{ $item->description }}</p>
                         @endif
 
-                        @if(method_exists($item, 'category'))
-                            <div class="card-text">
-                                <small class="text-muted">
-                                    Категория:
-                                    <a href="{{ $item->category->url() }}"
-                                       title="{{ $item->category->title }}" >{{ $item->category->title }}</a>
-                                </small>
-                            </div>
-                        @endif
+                        <div class="card-text">
+                            <small class="text-muted">
+                                Добавлено
+                                <time datetime="{{ $item->created_at }}">{{ \diffForHumans($item->created_at) }}</time>
+                            </small>
+                        </div>
 
                         <a href="{{ $item->url() }}" title="{{ $item->title }}" class="btn-link">Подробнее »</a>
 
