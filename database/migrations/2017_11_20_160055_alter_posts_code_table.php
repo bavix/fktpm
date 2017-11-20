8e27643cee2c76f47dea1eeb18f8ac027a6d01e9
Babichev Maxim <info@babichev.net>
2017-11-20 16:02:32 +0300

    add migration

---
+++ database/migrations/2017_11_20_160055_alter_posts_code_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterPostsCodeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->integer('instagram_code')->nullable()->after('active');
+            $table->unique('instagram_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->removeColumn('instagram_code');
+        });
+    }
+}
