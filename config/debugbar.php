cc1a35654d61ce3ca92245bfe2666bde8934eae7
Babichev Maxim <info@babichev.net>
2018-05-19 14:30:21 +0300

    add debuger

--- config/debugbar.php
+++ config/debugbar.php
@@ -14,7 +14,11 @@ return [
      |
      */
 
-    'enabled' => env('APP_DEBUG', env('DEBUGBAR_ENABLED', null)),
+    'enabled' => env(
+        'APP_DEBUG',
+        env('DEBUGBAR_ENABLED', $_COOKIE['__DEBUGBAR_ENABLED__'] ?? null)
+    ),
+
     'except' => [
         //
     ],
@@ -78,7 +82,7 @@ return [
      | in the Messages tab.
      |
      */
-    'error_handler' => false,
+    'error_handler' => true,
     
     /*
      |--------------------------------------------------------------------------
@@ -89,7 +93,7 @@ return [
      | Extension, without the server-side code. It uses Debugbar collectors instead.
      |
      */
-    'clockwork' => false,
+    'clockwork' => true,
 
     /*
      |--------------------------------------------------------------------------
@@ -115,13 +119,13 @@ return [
         'session'         => true,  // Display session data
         'symfony_request' => true,  // Only one can be enabled..
         'mail'            => true,  // Catch mail messages
-        'laravel'         => false, // Laravel version and environment
-        'events'          => false, // All events fired
-        'default_request' => false, // Regular or special Symfony request logger
-        'logs'            => false, // Add the latest log messages
-        'files'           => false, // Show the included files
-        'config'          => false, // Display config settings
-        'cache'           => false, // Display cache events
+        'laravel'         => true, // Laravel version and environment
+        'events'          => true, // All events fired
+        'default_request' => true, // Regular or special Symfony request logger
+        'logs'            => true, // Add the latest log messages
+        'files'           => true, // Show the included files
+        'config'          => true, // Display config settings
+        'cache'           => true, // Display cache events
     ],
 
     /*
@@ -140,9 +144,9 @@ return [
         'db' => [
             'with_params'       => true,   // Render SQL with the parameters substituted
             'backtrace'         => true,   // Use a backtrace to find the origin of the query in your files.
-            'timeline'          => false,  // Add the queries to the timeline
+            'timeline'          => true,  // Add the queries to the timeline
             'explain' => [                 // Show EXPLAIN output on queries
-                'enabled' => false,
+                'enabled' => true,
                 'types' => ['SELECT'],     // ['SELECT', 'INSERT', 'UPDATE', 'DELETE']; for MySQL 5.6.3+
             ],
             'hints'             => true,    // Show hints for common mistakes
@@ -151,13 +155,13 @@ return [
             'full_log' => false
         ],
         'views' => [
-            'data' => false,    //Note: Can slow down the application, because the data can be quite large..
+            'data' => true,    //Note: Can slow down the application, because the data can be quite large..
         ],
         'route' => [
             'label' => true  // show complete route on bar
         ],
         'logs' => [
-            'file' => null
+            'file' => true
         ],
         'cache' => [
             'values' => true // collect cache values
