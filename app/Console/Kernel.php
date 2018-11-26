994de1e86375ac7284ddcced88770a67f0061ca4
Babichev Maxim <maksim.babichev95@gmail.com>
2018-11-26 12:55:20 +0000

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->command('bx:instagram')
             ->timezone('Europe/Moscow')
-            ->cron('16 * * * *');
+            ->cron('6 * * * *');
     }
 
     /**
