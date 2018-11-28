111297cd92845f72986cd0abcb3bf75772963e47
Babichev Maxim <maksim.babichev95@gmail.com>
2018-11-28 07:04:08 +0000

    Update APIController.php

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -12,7 +12,7 @@ class APIController extends Controller
 
     public function blocks(Request $request)
     {
-        $currentPage = $request->input('page') ?: 1;
+        $currentPage = $request->input('page', 1);
         
         return BlockResource::collection(Cache::remember('tags_block_' . $currentPage, 60, function() { 
             return Tag::blocks()->paginate(100); 
