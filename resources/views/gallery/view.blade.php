402d8efddbb92ce9ba47d9e3a7025c76342e4c07
Babichev Maxim <maksim.babichev95@gmail.com>
2019-01-28 11:02:39 +0000

    Update view.blade.php

--- resources/views/gallery/view.blade.php
+++ resources/views/gallery/view.blade.php
@@ -1,7 +1,7 @@
 <div class="col-12">
     <ul class="lightGallery list-unstyled row">
         @foreach($item->images as $_picture)
-            <li style="padding-right: 1px; padding-left: 0; padding-bottom: 1px" class="col-4 col-sm-3 col-md-2" data-src="/storage/{{ $_picture->lg() }}">
+            <li style="padding-right: 1px; padding-left: 0; padding-bottom: 1px" class="col-4 col-sm-3 col-md-2" data-src="/storage/{{ $_picture->md() }}">
                 <a href="#">
                     <img class="img-responsive" data-src="/storage/{{ $_picture->sm() }}"
                         style="object-position: center; object-fit: none"
