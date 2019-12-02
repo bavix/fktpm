9a0777e1ae4a656f2ee4d876b3d7a6a5e1f37176
Babichev Maxim <info@babichev.net>
2019-12-02 11:28:20 +0300

    Update sape.php

--- helpers/sape.php
+++ helpers/sape.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('_SAPE_USER')) {
-    define('_SAPE_USER', 'afe4ffefe1832be3847f82a6a2363822');
+    define('_SAPE_USER', env('SAPE_KEY', 'SAPE'));
 }
 
 include_once dirname(__DIR__) . '/public/' . _SAPE_USER . '/sape.php';
