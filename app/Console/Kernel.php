067c1dd7b73de972e60264de500cf53c1a9acb8b
Babichev Maxim <maksim.babichev95@gmail.com>
2020-07-12 02:21:00 +0300

    fix prof -> entry

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('prof:bulk')
+        $schedule->command('entry:bulk')
             ->timezone('Europe/Moscow')
             ->everyMinute();
 
